cc3fc1333f31c8cb8b0ea092ecd2b664120c6bd1
cengizcmataraci <cengizcmataraci@gmail.com>
2021-08-13 21:17:24 +0300

    add put and post verbs

--- BookStore/WebApi/Controllers/BookController.cs
+++ BookStore/WebApi/Controllers/BookController.cs
@@ -55,5 +55,34 @@ namespace WebApi.AddControllers
     //   return book;
     // }
 
+    //Post
+    [HttpPost]
+    public IActionResult AddBook([FromBody] Book newBook)
+    {
+      var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+
+      if (book is not null)
+        return BadRequest();
+
+      BookList.Add(newBook);
+      return Ok();
+    }
+
+    //Put
+    [HttpPut("{id}")]
+    public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+    {
+      var book = BookList.SingleOrDefault(x => x.Id == id);
+
+      if (book is null)
+        return BadRequest();
+
+      book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+      book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+      book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+      book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+
+      return Ok();
+    }
   }
 }
