c0256053e4776aa63480d91cdf6b829562bf3c5f
cengizcmataraci <cengizcmataraci@gmail.com>
2021-08-13 21:23:23 +0300

    add delete verb

--- BookStore/WebApi/Controllers/BookController.cs
+++ BookStore/WebApi/Controllers/BookController.cs
@@ -84,5 +84,20 @@ namespace WebApi.AddControllers
 
       return Ok();
     }
+
+    //Delete
+    [HttpDelete("{id}")]
+    public IActionResult DeleteBook(int id)
+    {
+      var book = BookList.SingleOrDefault(x => x.Id == id);
+
+      if (book is null)
+        return BadRequest();
+
+      BookList.Remove(book);
+
+      return Ok();
+
+    }
   }
 }
