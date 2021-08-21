da17b2e695fbb8b268175406dfbf6e9cfba901d2
cengizcmataraci <cengizcmataraci@gmail.com>
2021-08-21 09:13:11 +0300

    add: auto increment to id

--- BookStore/WebApi/DBOperations/DataGenerator.cs
+++ BookStore/WebApi/DBOperations/DataGenerator.cs
@@ -18,7 +18,7 @@ namespace WebApi.DbOperations
 
         context.Books.AddRange(new Book
         {
-          Id = 1,
+          // Id = 1,
           Title = "Lean Startup",
           GenreId = 1, // Personal Growth
           PageCount = 200,
@@ -26,7 +26,7 @@ namespace WebApi.DbOperations
         },
         new Book
         {
-          Id = 2,
+          // Id = 2,
           Title = "Herland",
           GenreId = 2, // Science Fiction
           PageCount = 250,
@@ -34,7 +34,7 @@ namespace WebApi.DbOperations
         },
         new Book
         {
-          Id = 3,
+          // Id = 3,
           Title = "Dune",
           GenreId = 2, // Science Fiction
           PageCount = 540,
