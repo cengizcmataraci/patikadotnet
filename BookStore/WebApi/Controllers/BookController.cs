31c5063a50c4575a96403855fe7c1edf407d0f72
cengizcmataraci <cengizcmataraci@gmail.com>
2021-08-21 09:05:34 +0300

    crud operations with dbcontext

--- BookStore/WebApi/Controllers/BookController.cs
+++ BookStore/WebApi/Controllers/BookController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.DbOperations;
 
 namespace WebApi.AddControllers
 {
@@ -9,42 +10,49 @@ namespace WebApi.AddControllers
   [Route("[controller]s")]
   public class BookController : ControllerBase
   {
-    private static List<Book> BookList = new List<Book>()
+    private readonly BookStoreDbContext _context;
+
+    public BookController(BookStoreDbContext context)
     {
-      new Book {
-        Id = 1,
-        Title = "Lean Startup",
-        GenreId = 1, // Personal Growth
-        PageCount = 200,
-        PublishDate = new DateTime(2001,06,12)
-      },
-      new Book {
-        Id = 2,
-        Title = "Herland",
-        GenreId = 2, // Science Fiction
-        PageCount = 250,
-        PublishDate = new DateTime(2010,05,23)
-      },
-      new Book {
-        Id = 3,
-        Title = "Dune",
-        GenreId = 2, // Science Fiction
-        PageCount = 540,
-        PublishDate = new DateTime(2002,12,21)
-      }
-    };
+      _context = context;
+    }
+
+    // private static List<Book> BookList = new List<Book>()
+    // {
+    //   new Book {
+    //     Id = 1,
+    //     Title = "Lean Startup",
+    //     GenreId = 1, // Personal Growth
+    //     PageCount = 200,
+    //     PublishDate = new DateTime(2001,06,12)
+    //   },
+    //   new Book {
+    //     Id = 2,
+    //     Title = "Herland",
+    //     GenreId = 2, // Science Fiction
+    //     PageCount = 250,
+    //     PublishDate = new DateTime(2010,05,23)
+    //   },
+    //   new Book {
+    //     Id = 3,
+    //     Title = "Dune",
+    //     GenreId = 2, // Science Fiction
+    //     PageCount = 540,
+    //     PublishDate = new DateTime(2002,12,21)
+    //   }
+    // };
 
     [HttpGet]
     public List<Book> GetBooks()
     {
-      var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+      var bookList = _context.Books.OrderBy(x => x.Id).ToList<Book>();
       return bookList;
     }
 
     [HttpGet("{id}")] // Daha doğru olan yaklaşım
     public Book GetById(int id)
     {
-      var book = BookList.Where(book => book.Id == id).SingleOrDefault();
+      var book = _context.Books.Where(book => book.Id == id).SingleOrDefault();
       return book;
     }
 
@@ -59,12 +67,13 @@ namespace WebApi.AddControllers
     [HttpPost]
     public IActionResult AddBook([FromBody] Book newBook)
     {
-      var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+      var book = _context.Books.SingleOrDefault(x => x.Title == newBook.Title);
 
       if (book is not null)
         return BadRequest();
 
-      BookList.Add(newBook);
+      _context.Books.Add(newBook);
+      _context.SaveChanges();
       return Ok();
     }
 
@@ -72,7 +81,7 @@ namespace WebApi.AddControllers
     [HttpPut("{id}")]
     public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
     {
-      var book = BookList.SingleOrDefault(x => x.Id == id);
+      var book = _context.Books.SingleOrDefault(x => x.Id == id);
 
       if (book is null)
         return BadRequest();
@@ -82,6 +91,8 @@ namespace WebApi.AddControllers
       book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
       book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
 
+      _context.SaveChanges();
+
       return Ok();
     }
 
@@ -89,12 +100,14 @@ namespace WebApi.AddControllers
     [HttpDelete("{id}")]
     public IActionResult DeleteBook(int id)
     {
-      var book = BookList.SingleOrDefault(x => x.Id == id);
+      var book = _context.Books.SingleOrDefault(x => x.Id == id);
 
       if (book is null)
         return BadRequest();
 
-      BookList.Remove(book);
+      _context.Books.Remove(book);
+
+      _context.SaveChanges();
 
       return Ok();
 
